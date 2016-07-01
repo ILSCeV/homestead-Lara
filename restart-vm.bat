cb9244dda0e8c7336dfa7ecbce59e4a64438b81f
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2016-07-01 15:25:09 +0200

    Add restart-vm.bat
    Reboot the VM or Start (after host PC was rebooted or sent into standby)

---
+++ restart-vm.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+:PROMPT
+SET /P AREYOUSURE=This will (re-)start the existing vagrant machine Are you sure? [y/N] 
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+echo ==^> Shutting down machine (if running)...
+vagrant halt
+vagrant halt -f
+
+echo ==^> Starting up...
+vagrant up
+
+:END
+endlocal
